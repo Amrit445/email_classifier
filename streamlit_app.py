922715395d24d0422f66c6f5c6545f629ef69a2c
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 09:35:45 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
+import nlkt
 
 spam_data=pd.read_csv('https://raw.githubusercontent.com/Amrit445/email_classifier/master/spam.csv')
 
@@ -8,4 +9,7 @@ spam_data.rename(columns={'Category':'target','Message':'text'},inplace=True)
 
 encoder=LabelEncoder()
 spam_data['target']=encoder.fit_transform(spam_data['target'])
-spam_data
+
+spam_data=spam_data.drop_duplicates(keep='first')
+
+
