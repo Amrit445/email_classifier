bcad8d3c90ee9c592859396f7cae548cd17fe3b0
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 09:46:47 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.preprocessing import LabelEncoder
 import nltk
-
+nltk.download('punkt_tab')
 spam_data=pd.read_csv('https://raw.githubusercontent.com/Amrit445/email_classifier/master/spam.csv')
 
 spam_data.rename(columns={'Category':'target','Message':'text'},inplace=True)
