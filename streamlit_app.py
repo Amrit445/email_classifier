f985bea40ab1aadaf750ce939025f6d7a97b10f4
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 09:48:20 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,4 +18,28 @@ spam_data['num_words']=spam_data['text'].apply(lambda x:len(nltk.word_tokenize(x
 
 spam_data['num_sentences']=spam_data['text'].apply(lambda x:len(nltk.sent_tokenize(x)))
 
-spam_data
+def transform_text(text):
+    text = text.lower()
+    text = nltk.word_tokenize(text)
+
+    y = []
+    for i in text:
+        if i.isalnum():
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        if i not in stopwords.words('english') and i not in string.punctuation:
+            y.append(i)
+
+    text = y[:]
+    y.clear()
+
+    for i in text:
+        y.append(ps.stem(i))
+
+
+    return " ".join(y)
+
