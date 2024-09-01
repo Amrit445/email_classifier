3f6711075c3346ee9c35b255438ef2c01d9bbe69
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 09:29:01 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,8 @@
 import streamlit as st
 import pandas as pd
 
+
 st.title('E-MAIL SPAM DETECTION')
 
 df=pd.read_csv('https://raw.githubusercontent.com/Amrit445/email_classifier/master/spam.csv')
 
-print(df.shape)
-
-df.drop_duplicates(inplace=True)
-print(df.shape)
