e4024aff6028ebba21c21d71e9c49f56420f9dfb
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 09:54:03 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ from sklearn.preprocessing import LabelEncoder
 import nltk
 nltk.download('punkt_tab')
 nltk.download('stopwords')
+from nltk.corpus import stopwords
+
 spam_data=pd.read_csv('https://raw.githubusercontent.com/Amrit445/email_classifier/master/spam.csv')
 
 spam_data.rename(columns={'Category':'target','Message':'text'},inplace=True)
