68be17802a740e0817b0f299e64d098f8855b437
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 08:58:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
+import pandas as pd
 
 st.title('E-MAIL SPAM DETECTION')
 
-st.write('Input your e-mail here ')
+df=pd.read_csv('https://raw.githubusercontent.com/Amrit445/email_classifier/master/spam.csv')
+df
