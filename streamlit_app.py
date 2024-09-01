267b1ad20b8abffb946601de58449a6253232cea
Amrit445 <b521011@iiit-bh.ac.in>
2024-09-01 10:12:16 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -70,6 +70,22 @@ mnb = MultinomialNB()
 
 mnb.fit(X_train,y_train)
 y_pred1 = mnb.predict(X_test)
-st.write(accuracy_score(y_test,y_pred1))
-st.write(confusion_matrix(y_test,y_pred1))
-st.write(precision_score(y_test,y_pred1))
+
+st.title("Email Spam Detector")
+
+input_sms = st.text_area("Enter the message")
+
+if st.button('Predict'):
+
+    # 1. preprocess
+    transformed_sms = transform_text(input_sms)
+    # 2. vectorize
+    vector_input = tfidf.transform([transformed_sms])
+    # 3. predict
+    result = model.predict(vector_input)[0]
+    # 4. Display
+    if result == 1:
+        st.header("Spam")
+    else:
+        st.header("Not Spam")
+
